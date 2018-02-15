00cd2116a23e990678622c3dae5346817c41765f
vegter <amsjurien@gmail.com>
2018-02-15 09:01:37 +0100

    GGW Selector basic layout

--- src/components/GGWSelector.vue
+++ src/components/GGWSelector.vue
@@ -3,24 +3,60 @@
   <div class="alert">
     <div class="row">
       <div class="col-sm">
-        <div class="row">
-          <div class="col-sm-4">Gebied</div>
-          <div class="col-sm-8" v-if="gebied">> <span class="selected-value">{{gebied.naam}}</span></div>
-        </div>
-        <div class="row">
-          <div class="col-sm-4">Wijk</div>
-          <div class="col-sm-8" v-if="wijk">> <span class="selected-value">{{wijk.naam}}</span></div>
-        </div>
-        <div class="row">
-          <div class="col-sm-4">Buurt</div>
-          <div class="col-sm-8" v-if="buurt">> <span class="selected-value">{{buurt.naam}}</span></div>
-        </div>
+        <b-form inline class="row">
+          <div class="col-sm-3">
+            <label for="selectGebied">Gebied</label>
+          </div>
+          <div class="col-sm-9" v-if="selection.gebieden">
+            <b-form-select v-model="selection.gebied"
+                           :options="selection.gebieden"
+                           text-field="naam"
+                           value-field="code"
+                           id="selectGebied">
+            </b-form-select>
+          </div>
+        </b-form>
+        <b-form inline class="row">
+          <div class="col-sm-3">
+            <label for="selectWijk">Wijk</label>
+          </div>
+          <div class="col-sm-9" v-if="selection.wijken">
+            <b-form-select v-model="selection.wijk"
+                           :options="selection.wijken"
+                           text-field="naam"
+                           value-field="code"
+                           id="selectWijk">
+            </b-form-select>
+          </div>
+        </b-form>
+        <b-form inline class="row">
+          <div class="col-sm-3">
+            <label for="selectBuurt">Buurt</label>
+          </div>
+          <div class="col-sm-9" v-if="selection.buurten">
+            <b-form-select v-model="selection.buurt"
+                           :options="selection.buurten"
+                           text-field="naam"
+                           value-field="code"
+                           id="selectBuurt">
+            </b-form-select>
+          </div>
+        </b-form>
       </div>
       <div class="col-sm">
-        <div class="row">
-          <div class="col-sm-4">Thema</div>
-          <div class="col-sm-8">> <span class="selected-value">Gebied in het kort</span></div>
-        </div>
+        <b-form inline class="row">
+          <div class="col-sm-3">
+            <label for="selectThema">Thema</label>
+          </div>
+          <div class="col-sm-9" v-if="selection.themas">
+            <b-form-select v-model="selection.thema"
+                           :options="selection.themas"
+                           text-field="naam"
+                           value-field="code"
+                           id="selectThema">
+            </b-form-select>
+          </div>
+        </b-form>
       </div>
     </div>
   </div>
@@ -29,6 +65,9 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import util from '../services/util'
+
+const DEFAULT_GEBIED = 'DX01'
 
 export default {
   name: 'GGWSelector',
@@ -36,6 +75,16 @@ export default {
   },
   data () {
     return {
+      selection: {
+        gebieden: null,
+        gebied: null,
+        wijken: null,
+        wijk: null,
+        buurten: null,
+        buurt: null,
+        themas: null,
+        thema: null
+      }
     }
   },
   computed: {
@@ -50,15 +99,13 @@ export default {
     })
   },
   watch: {
+  },
+  async mounted () {
+    this.selection.gebieden = await util.getGebieden()
+    this.selection.gebied = this.selection.gebieden.find(g => g.code === DEFAULT_GEBIED).code
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  @import "../../static/ams";
-
-  .selected-value {
-    color: $ams-rood;
-    font-weight: bold;
-  }
 </style>
