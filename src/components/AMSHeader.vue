26699a8a195ad710e0b4af51894c2fdf28bb2ecf
vegter <amsjurien@gmail.com>
2018-02-19 15:54:41 +0100

    Show map in header

--- src/components/AMSHeader.vue
+++ src/components/AMSHeader.vue
@@ -1,10 +1,27 @@
 <!--Example of a component that uses D3-->
 <template>
-  <header>
-    <img src="../../static/logo-tall.svg">
-    <h1>Kerncijfers gebiedsgericht werken</h1>
-  </header>
+  <div class="row">
+    <div class="col-sm-9">
+      <header>
+        <img src="../../static/logo-tall.svg">
+        <h1 class="text-center">Kerncijfers gebiedsgericht werken</h1>
+      </header>
+    </div>
+    <div class="col-sm-3">
+      <leaflet-example></leaflet-example>
+    </div>
+  </div>
+
 </template>
 
 <script>
+import leafletExample from './LeafletExample'
+
+export default {
+  name: 'AMSHeader',
+  components: {
+    'leaflet-example': leafletExample
+  }
+}
+
 </script>
