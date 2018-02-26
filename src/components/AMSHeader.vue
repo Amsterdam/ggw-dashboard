4afa7b7d55285862f939664ee4a87b2caf71af50
vegter <amsjurien@gmail.com>
2018-02-26 14:57:06 +0100

    Color legend in header

--- src/components/AMSHeader.vue
+++ src/components/AMSHeader.vue
@@ -1,7 +1,7 @@
 <!--Example of a component that uses D3-->
 <template>
   <div class="row">
-    <div class="col-sm-9">
+    <div class="col-sm-6">
       <header>
         <img src="../../static/logo-tall.svg">
         <h1 class="text-center">Gebied in beeld</h1>
@@ -10,17 +10,22 @@
     <div class="col-sm-3">
       <gwb-map></gwb-map>
     </div>
+    <div class="col-sm-3">
+      <color-legend></color-legend>
+    </div>
   </div>
 
 </template>
 
 <script>
 import gwbMap from './GWBMap'
+import colorLegend from './ColorLegend'
 
 export default {
   name: 'AMSHeader',
   components: {
-    'gwb-map': gwbMap
+    'gwb-map': gwbMap,
+    'color-legend': colorLegend
   }
 }
 
---
+++ src/components/ColorLegend.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>Betekenis kleuren:</div>
+    <div v-for="entry in legend" :key="entry.color" class="legend-entry">
+      <span :style="getBulletStyle(entry)" class="legend-bullet">&#9634;</span>
+      {{entry.text}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { CATEGORY_COLORS, COLOR } from '../services/colorcoding'
+
+export default {
+  name: 'ColorLegend',
+  components: {
+  },
+  props: [
+  ],
+  data () {
+    return {
+      legend: CATEGORY_COLORS
+    }
+  },
+  computed: {
+  },
+  methods: {
+    getBulletStyle (entry) {
+      const style = {
+        'background-color': entry.color,
+        color: entry.color
+      }
+      if (entry.color === COLOR['ams-wit']) {
+        style['border-color'] = COLOR['ams-zwart']
+      }
+      return style
+    }
+  },
+  watch: {
+  },
+  created () {
+  }
+}
+
+</script>
+
+<style>
+  .legend-entry {
+    margin-top: 7px;
+  }
+
+  .legend-bullet {
+    letter-spacing: 2px;
+    border: 1px solid;
+    margin-right: 7px;
+  }
+</style>
