3dfc6f14ddd47ca7254cfdd15d3e7c75d6f4a65a
vegter <amsjurien@gmail.com>
2018-03-07 14:19:48 +0100

    tooltip pie and woonvormen

--- src/components/Woonvormen.vue
+++ src/components/Woonvormen.vue
@@ -1,22 +1,25 @@
 <template>
   <ul>
     <li v-for="(item, index) in data" :key="index">
+      <tooltip :cijfers="data" :cijfer="item">
       {{item.label}}
-      <span v-if="item.recent"
-        v-b-tooltip.hover v-b-tooltip.click v-b-tooltip.left title="">
+      <span v-if="item.recent">
         {{item.recent | displaywaarde}}
       </span>
+      </tooltip>
     </li>
   </ul>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import tooltip from './Tooltip'
 import util from '../services/util'
 
 export default {
   name: 'Woonvormen',
   components: {
+    'tooltip': tooltip
   },
   props: [
     'config'
