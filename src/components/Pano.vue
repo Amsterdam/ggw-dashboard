6a8343ddb0d75ee564922f0c1ca19fa2b47cc581
vegter <amsjurien@gmail.com>
2018-03-14 14:48:49 +0100

    Update in het kort voor cards en margins Update panos

--- src/components/Pano.vue
+++ src/components/Pano.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <img v-if="url" :src="url" height="300px">
+    <img v-if="url" :src="url">
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import panos from '../../static/links/panos'
+import util from '../services/util'
 
 let PANO_DEFAULT = null
 
@@ -34,7 +35,24 @@ export default {
         (this.wijk && panos.find(p => p.gwb === this.wijk.volledige_code)) ||
         (this.gebied && panos.find(p => p.gwb === this.gebied.volledige_code)) ||
         PANO_DEFAULT
-      this.url = pano && pano.pano
+      const panoUrl = pano && pano.pano
+
+      // https://api.data.amsterdam.nl/panorama/thumbnail/TMX7316010203-000227_pano_0000_001160/?width=400&heading=135
+      // https://data.amsterdam.nl/#?mpb=topografie&mpz=9&mpo=pano::T&mpv=52.4147109:4.8858658&sbf=BS&sbh=-FR&sbi=TMX7316010203-000317_pano_0000_004374&sbl=ZTGzH:3J5Kv&sbp=13
+
+      let sbi, sbh
+      try {
+        sbi = panoUrl.match(/&sbi=([^&]*)/)[1]
+        sbh = panoUrl.match(/&sbh=([^&]*)/)[1]
+        sbh = Math.round(util.base62DecodeAngle(sbh, 1))
+      } catch (error) {
+        console.error('Missing pano for', this.gwb.volledige_code)
+        sbi = 'TMX7316010203-000353_pano_0002_000059'
+        sbh = 205
+      }
+      const width = 500
+      const url = `https://api.data.amsterdam.nl/panorama/thumbnail/${sbi}/?width=${width}&heading=${sbh}`
+      this.url = url
     }
   },
   watch: {
@@ -56,4 +74,8 @@ export default {
 </script>
 
 <style scoped>
+  img {
+    margin-top: 5px;
+    width: 100%;
+  }
 </style>
