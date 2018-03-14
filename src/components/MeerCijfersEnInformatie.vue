18ca4e6fb95e56b0c16deaa1b718277e59ee1967
vegter <amsjurien@gmail.com>
2018-03-14 15:41:00 +0100

    Colofon en disclaimer

--- src/components/MeerCijfersEnInformatie.vue
+++ src/components/MeerCijfersEnInformatie.vue
@@ -10,7 +10,7 @@
         </ul>
       </div>
     </div>
-    <div class="grid-blok grid_6 lijst">
+    <div class="grid-blok grid_6">
       <div class="grid-element">
         <h2 class="blok-titel">Meer cijfers</h2>
         <ul class="links">
@@ -18,6 +18,33 @@
             <a :href="info.url" class="outsite" target="_blank" rel="noreferrer"><span class="linklabel">{{info.label}}</span></a>
           </li>
         </ul>
+      </div>
+    </div>
+    <div class="grid-blok grid_12">
+      <div class="grid-element">
+        <h2 class="blok-titel">Colofon</h2>
+        <p>
+        Gebied in Beeld is gemaakt door OIS in opdracht van de samenwerkende stadsdelen.<br>
+        Contact: <a href="mailto:algemeen.OIS@amsterdam.nl">algemeen.OIS@amsterdam.nl</a>
+        </p>
+      </div>
+    </div>
+    <div class="grid-blok grid_12">
+      <div class="grid-element">
+        <h2 class="blok-titel">Disclaimer</h2>
+        <p>
+        De inhoud van Gebied in Beeld is met uiterste zorgvuldigheid tot stand gebracht.
+        De inhoud wordt regelmatig gecontroleerd en geactualiseerd.
+        OIS kan echter niet aansprakelijk worden gesteld voor de juistheid, volledigheid en actualiteit van de website.
+        OIS kan in het bijzonder niet aansprakelijk worden gesteld voor eventuele schade of consequenties ontstaan door direct of indirect gebruik van de inhoud van de website.
+        </p>
+        <p>
+        Alle informatie op Gebied in Beeld valt onder Copyright © van OIS, tenzij een andere bron is vermeld.<br>
+        Het overnemen van gegevens is met de volgende bronvermelding toegestaan: <b>OIS Amsterdam</b>.
+        </p>
+        <p>
+          Wilt u informatie hergebruiken, neem dan even contact op met OIS via <a href="mailto:algemeen.OIS@amsterdam.nl">algemeen.OIS@amsterdam.nl</a>.
+        </p>
       </div>
     </div>
   </div>
