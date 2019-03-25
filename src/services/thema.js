64713f6f31d5d8a404e8f36ec5c70168961c7341
JJM van Deursen <janjaap@users.noreply.github.com>
2019-03-25 13:13:05 +0100

    Removes theme specific lines

--- src/services/thema.js
+++ src/services/thema.js
@@ -17,7 +17,6 @@ export const STEDELIJKE_ONTWIKKELING_EN_WONEN = 'Stedelijke ontwikkeling en wone
 export const VERKEER_EN_OPENBARE_RUIMTE = 'Verkeer en Openbare ruimte'
 export const WELZIJN_ZORG_EN_SPORT = 'Welzijn, zorg en sport'
 export const WERK_INKOMEN_EN_PARTICIPATIE = 'Werk, inkomen en participatie'
-export const SOCIALE_KRACHT = 'Sociale kracht'
 
 /**
  * The total set of available themas of which the user can choose from
@@ -33,8 +32,7 @@ export const THEMAS = [
   OPENBARE_ORDE_EN_VEILIGHEID,
   ONDERWIJS_JEUGD_EN_DIVERSITEIT,
   WERK_INKOMEN_EN_PARTICIPATIE,
-  WELZIJN_ZORG_EN_SPORT,
-  SOCIALE_KRACHT
+  WELZIJN_ZORG_EN_SPORT
 ]
 
 /**
