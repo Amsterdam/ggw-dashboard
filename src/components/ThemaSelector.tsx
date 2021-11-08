20e0db56c2315a9e7b04f015878d7437da5e4e90
janthijs <92784122+janthijs@users.noreply.github.com>
2021-11-08 10:09:38 +0100

    fix: add theme selector

--- src/components/ThemaSelector.tsx
+++ src/components/ThemaSelector.tsx
@@ -1,5 +1,37 @@
+import { FormEvent } from "react";
+import styled from "styled-components";
+import { Select, Row, Column, themeSpacing } from "@amsterdam/asc-ui";
+
+import { THEMAS } from "../services/thema";
+
+const SpacingDiv = styled("div")`
+  padding-top: ${themeSpacing(3)};
+`;
+
 const ThemaSelector = () => {
-  return <div> ThemaSelector </div>;
+  return (
+    <Row>
+      <Column span={3}>
+        <SpacingDiv>
+          <Select
+            id="thema"
+            label="Thema"
+            value={""}
+            onChange={(event: FormEvent<HTMLSelectElement>) => {
+              console.log(event);
+            }}
+          >
+            {THEMAS &&
+              THEMAS.map((t, i) => (
+                <option key={t} value={i}>
+                  {t}
+                </option>
+              ))}
+          </Select>
+        </SpacingDiv>
+      </Column>
+    </Row>
+  );
 };
 
 export default ThemaSelector;
