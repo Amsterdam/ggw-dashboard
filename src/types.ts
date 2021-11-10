de30f6951eb6b3db4015412821f473450d533935
Jasper Swart <jasper.g.swart@gmail.com>
2021-11-10 16:36:23 +0100

    added separate type def

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+/**
+ * All types definition
+ */
+
+export interface StdType {
+    indicatorDefinitieId: string
+    gemiddelde: number
+    jaar: number
+    standaardafwijking: number
+  }
+  
+  export interface MetaType {
+    indicatorDefinitieId: string
+    kleurenpalet: number
+  }
+  
