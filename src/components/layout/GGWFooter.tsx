8808cd860aea3ea9db62aad1242ce2ae6c35b811
janthijs <92784122+janthijs@users.noreply.github.com>
2021-11-24 10:31:03 +0100

    fix: update config

--- src/components/layout/GGWFooter.tsx
+++ src/components/layout/GGWFooter.tsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
-import { Footer, FooterTop, Row, Column, FooterSection, List, ListItem, Link, themeSpacing } from "@amsterdam/asc-ui";
-
-import meerInformatie from "../../static/links/meer_informatie.json";
-import meerCijfers from "../../static/links/meer_cijfers.json";
+import { Footer, FooterTop, Row, Column, FooterSection, themeSpacing } from "@amsterdam/asc-ui";
 
 const SpacerDiv = styled.div`
   margin-top: ${themeSpacing(18)};
@@ -14,30 +11,33 @@ const GGWFooter = () => {
       <Footer>
         <FooterTop>
           <Row>
-            <Column span={6}>
-              <FooterSection title="Meer informatie">
-                <List>
-                  {meerInformatie.map((link) => (
-                    <ListItem key={link.label}>
-                      <Link darkBackground href={link.url} inList>
-                        {link.label}
-                      </Link>
-                    </ListItem>
-                  ))}
-                </List>
+            <Column span={12}>
+              <FooterSection title="Colofoon">
+                <p>
+                  Gebied in Beeld is gemaakt door OIS in opdracht van de samenwerkende stadsdelen. Contact:{" "}
+                  <a href="mailto:algemeen.OIS@amsterdam.nl">algemeen.OIS@amsterdam.nl</a>
+                </p>
               </FooterSection>
             </Column>
-            <Column span={6}>
-              <FooterSection title="Meer cijfers">
-                <List>
-                  {meerCijfers.map((link) => (
-                    <ListItem key={link.label}>
-                      <Link darkBackground href={link.url} inList>
-                        {link.label}
-                      </Link>
-                    </ListItem>
-                  ))}
-                </List>
+            <Column span={12}>
+              <FooterSection title="Disclaimer">
+                <p>
+                  De inhoud van Gebied in Beeld is met uiterste zorgvuldigheid tot stand gebracht. De inhoud wordt
+                  regelmatig gecontroleerd en geactualiseerd. OIS kan echter niet aansprakelijk worden gesteld voor de
+                  juistheid, volledigheid en actualiteit van de website. OIS kan in het bijzonder niet aansprakelijk
+                  worden gesteld voor eventuele schade of consequenties ontstaan door direct of indirect gebruik van de
+                  inhoud van de website.
+                </p>
+
+                <p>
+                  Alle informatie op Gebied in Beeld valt onder Copyright © van OIS, tenzij een andere bron is vermeld.
+                  Het overnemen van gegevens is met de volgende bronvermelding toegestaan: OIS Amsterdam.
+                </p>
+
+                <p>
+                  Wilt u informatie hergebruiken, neem dan even contact op met OIS via{" "}
+                  <a href="mailto:algemeen.OIS@amsterdam.nl">algemeen.OIS@amsterdam.nl</a>.
+                </p>
               </FooterSection>
             </Column>
           </Row>
