5384ce1a5c8de5c1370f73314839d93e75209f44
janthijs <92784122+janthijs@users.noreply.github.com>
2021-11-24 12:09:30 +0100

    fix: bug in legend table where wrong year was used

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -6,7 +6,7 @@ import { Spinner } from "@amsterdam/asc-ui";
 import util from "../services/util";
 
 import { getColorsUsingStaticDefinition } from "../services/colorcoding";
-import vegaSpec from "../static/charts/linechart.json";
+import vegaSpec from "../static/charts/linechart";
 
 const vegaEmbedOptions = {
   actions: false,
@@ -15,32 +15,39 @@ const vegaEmbedOptions = {
 const LineChart = ({ title, gwb, config }) => {
   const chartRef = useRef<HTMLDivElement>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [showError, setShowError] = useState(false);
 
   async function updateData() {
     setIsLoading(true);
+    setShowError(false);
     const colors = getColorsUsingStaticDefinition(config);
     const chartdata = await util.getConfigCijfers(gwb, config);
     const chartBase = cloneDeep(vegaSpec);
 
-    const cijfers = util.flatten(
-      chartdata.map((data) =>
-        data.cijfers.map((cijfer) => ({
-          x: cijfer.jaar,
-          y: cijfer.waarde,
-          variable: data.label,
-          dash: /prognose/i.test(data.label), // show prognose variables as dashed lines
-        })),
-      ),
-    );
+    const cijfers = chartdata
+      .map((data) =>
+        data.cijfers
+          .filter((cijfer) => cijfer.waarde !== null)
+          .map((cijfer) => ({
+            x: cijfer.jaar,
+            y: cijfer.waarde,
+            variable: data.label,
+            dash: /prognose/i.test(data.label), // show prognose variables as dashed lines
+          })),
+      )
+      .flat();
 
     vegaSpec.data[0].values = cijfers;
     vegaSpec.scales[2].range = colors;
 
     // console.log(JSON.stringify(chartBase));
 
-    if (chartRef.current) {
+    if (chartRef.current && vegaSpec.data[0].values.length > 0) {
       setIsLoading(false);
       vegaEmbed(chartRef.current, chartBase, vegaEmbedOptions);
+    } else {
+      setIsLoading(false);
+      setShowError(true);
     }
   }
 
@@ -58,6 +65,7 @@ const LineChart = ({ title, gwb, config }) => {
       <h5 className="text-center">{title}</h5>
       <div className="chart-container">
         {isLoading ? <Spinner /> : null}
+        {showError && <p>Op dit schaalniveau is helaas geen informatie beschikbaar.</p>}
         <div ref={chartRef}></div>
       </div>
     </div>
