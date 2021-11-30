7e7e4927311724be60a212837abf05838c0eb8f0
Jasper Swart <jasper.g.swart@gmail.com>
2021-11-30 16:11:53 +0100

    fixed lint

--- src/services/apis/bbga.js
+++ src/services/apis/bbga.js
@@ -134,7 +134,7 @@ async function getCijfers(meta, year = null, gebiedCode = null, indicatorDefinit
     gebiedcode15: c.gebiedcode15,
     ...getColor(
       {
-        indicatorDefinitieId: meta?.indicatorDefinitieId ? meta?.indicatorDefinitieId : indicatorDefinitieId,
+        indicatorDefinitieId: meta?.indicatorDefinitieId,
         kleurenpalet: meta?.kleurenpalet ? meta.kleurenpalet : "",
       },
       c.waarde,
