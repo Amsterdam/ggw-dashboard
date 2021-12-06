b0dc5a8842299fa6ad087127148c5c316e235f45
Jasper Swart <jasper.g.swart@gmail.com>
2021-12-06 12:30:16 +0100

    component Verschillen part 1

--- src/components/Verschillen.tsx
+++ src/components/Verschillen.tsx
@@ -2,47 +2,34 @@ import { useState } from "react";
 import { Column, Row } from "@amsterdam/asc-ui";
 import VerschillenBarChart from "./VerschillenBarChart"
 import VerschillenMap from "./VerschillenMap"
-
+import HeaderRow from "../components/layout/HeaderRow";
 
 const Verschillen = ({ gwb }) => {
-  const [variabele, setVariabele] = useState('');
+  const [variabele, setVariabele] = useState<string>('');
   setVariabele("ORVERLOED_P");
 
-  // useEffect(() => {
-  //   if (!gwb) {
-  //     return;
-  //   }
-
-  //   // updateData();
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [gwb]);
-
-  // useEffect(() => {
-  //   if (!gwb) {
-  //     return;
-  //   }
-
-  //   // updateData();
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
   return (
-    <Row>
-      <Column span={6}>
-        <VerschillenMap 
-          gwb={gwb}
-          variabele={variabele}
-        />
-      </Column>
-      <Column span={6}>
+    <>
+      <Row>
+        <Column span={12}>
+          <HeaderRow title={`De verschillen in ${gwb?.naam}`} />
+        </Column>
+      </Row>
+      <Row>
+        <Column span={6}>
+          <VerschillenMap 
+            gwb={gwb}
+            variabele={variabele}
+          />
+        </Column>
+        <Column span={6}>
           <VerschillenBarChart 
             gwb={gwb}
             variabele={variabele}
           />
-      </Column>
-    </Row>
+        </Column>
+      </Row>
+    </>
   )
 };
 
