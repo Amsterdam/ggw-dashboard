1b778c89063412c6a07f922c1ba48aad971437d6
Jasper Swart <jasper.g.swart@gmail.com>
2021-12-14 14:06:16 +0100

    added more attributes to gwb

--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,13 @@
  */
 
 export interface Gwb {
-  code?: string;
-  display: string;
-  gebiedType: string;
+  code: string;
+  display?: string;
+  gebiedType?: string;
   naam: string;
   vollcode?: string;
+  volledige_code?: string;
+  geometrie?: any;
 }
 
 export interface StdType {
