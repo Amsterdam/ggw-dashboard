5a0655d35d088a6b58fddc10bf9e28ffb267df12
janthijs <92784122+janthijs@users.noreply.github.com>
2021-12-21 15:19:10 +0100

    fix: open in new tab

--- src/components/GGWInformation.tsx
+++ src/components/GGWInformation.tsx
@@ -35,19 +35,28 @@ const GGWInformation = () => {
               dit bestand zijn nog meer indicatoren, gebiedsindelingen en jaren opgenomen. Met de volgende applicaties
               maakt u makkelijk zelf een tabel, kaart of grafiek van de gegevens in het BBGA:
             </p>
-            <Link href="https://public.tableau.com/app/profile/ois.amsterdam/viz/Kerncijfertabel_ggw/Dashtabel">
+            <Link
+              href="https://public.tableau.com/app/profile/ois.amsterdam/viz/Kerncijfertabel_ggw/Dashtabel"
+              target="_blank"
+            >
               <ExternalLink width="18" height="18" />
               Tabel maken
             </Link>
-            <Link href="https://public.tableau.com/app/profile/ois.amsterdam/viz/Kerncijfertijdlijnen_ggw/Dashtijdlijnen">
+            <Link
+              href="https://public.tableau.com/app/profile/ois.amsterdam/viz/Kerncijfertijdlijnen_ggw/Dashtijdlijnen"
+              target="_blank"
+            >
               <ExternalLink width="18" height="18" />
               Grafiek maken
             </Link>
-            <Link href="https://public.tableau.com/app/profile/ois.amsterdam/viz/Kerncijfersggwkaart/Dashkaart">
+            <Link
+              href="https://public.tableau.com/app/profile/ois.amsterdam/viz/Kerncijfersggwkaart/Dashkaart"
+              target="_blank"
+            >
               <ExternalLink width="18" height="18" />
               Kaart maken
             </Link>
-            <Link href="https://onderzoek.amsterdam.nl/dataset/basisbestand-gebieden-amsterdam-bbga">
+            <Link href="https://onderzoek.amsterdam.nl/dataset/basisbestand-gebieden-amsterdam-bbga" target="_blank">
               <ExternalLink width="18" height="18" />
               Het BBGA downloaden
             </Link>
