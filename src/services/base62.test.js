51509c97f9989047da778b544c17ca9cb010e9d6
Jasper Swart <jasper.g.swart@gmail.com>
2021-12-24 08:08:37 +0100

    added unit test

--- src/services/base62.test.js
+++ src/services/base62.test.js
@@ -1,8 +1,10 @@
-import { decodeString, base62DecodeAngle } from "../services/base62";
+import { decodeString, base62DecodeAngle } from "./base62";
 
 describe("base62", () => {
   it("can base62 decode strings", () => {
     expect(decodeString("FR")).toEqual(957);
+  });
+  it("can base62 decode strings with angle", () => {
     expect(base62DecodeAngle("FR", 1)).toEqual(95.7);
     expect(base62DecodeAngle("-FR", 1)).toEqual(264.3);
     expect(base62DecodeAngle("0", 1)).toEqual(0);
