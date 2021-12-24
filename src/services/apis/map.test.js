bef967b72a7a30b5e0b63ff7a401494399f108f3
Jasper Swart <jasper.g.swart@gmail.com>
2021-12-24 16:43:24 +0100

    fixed unit test oxios get mock

--- src/services/apis/map.test.js
+++ src/services/apis/map.test.js
@@ -3,11 +3,10 @@ import { getGeometriesGeoJson } from "./map";
 import { gebieden } from "./map.fixtures";
 
 jest.mock("axios");
-console.log("gebieden", gebieden);
 
 describe("map", () => {
   it("should return all gebied types of gebieden", async () => {
-    axios.get.mockResolvedValue(gebieden);
+    axios.get.mockImplementationOnce(() => Promise.resolve({ data: gebieden }));
 
     const result = await getGeometriesGeoJson("Gebied");
     expect(result.features.length).toEqual(22);
