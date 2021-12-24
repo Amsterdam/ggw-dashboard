912c420fc01ed4322726a917a75704aa1dc06ff9
Jasper Swart <jasper.g.swart@gmail.com>
2021-12-24 17:04:51 +0100

    added more tests

--- src/services/apis/map.test.js
+++ src/services/apis/map.test.js
@@ -1,14 +1,28 @@
 import axios from "axios";
 import { getGeometriesGeoJson } from "./map";
-import { gebieden } from "./map.fixtures";
+import { stadsdelen, gebieden, wijken } from "./map.fixtures";
 
 jest.mock("axios");
 
 describe("map", () => {
+  it("should return all gebied types of stadsdelen", async () => {
+    axios.get.mockImplementationOnce(() => Promise.resolve({ data: stadsdelen }));
+
+    const result = await getGeometriesGeoJson("Stadsdeel");
+    expect(result.features.length).toEqual(9);
+  });
+
   it("should return all gebied types of gebieden", async () => {
     axios.get.mockImplementationOnce(() => Promise.resolve({ data: gebieden }));
 
     const result = await getGeometriesGeoJson("Gebied");
     expect(result.features.length).toEqual(22);
+  });
+
+  it("should return all gebied types of wijken", async () => {
+    axios.get.mockImplementationOnce(() => Promise.resolve({ data: wijken }));
+
+    const result = await getGeometriesGeoJson("Wijk");
+    expect(result.features.length).toEqual(107);
   });
 });
